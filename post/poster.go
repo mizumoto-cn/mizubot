5ed976d1b73d75a100b3f1f0ced8b5b8da475f0e
XU Ruiyuan <mizumoto@mizumoto.tech>
2024-10-03 17:30:39 +0900

    .

--- post/poster.go
+++ post/poster.go
@@ -78,7 +78,7 @@ func (p *poster) Post(ctx context.Context) error {
 
 	// Check for non-200 status code
 	if resp.StatusCode != http.StatusOK {
-		log.Printf("failed to post message, request: %v, status code: %d\n", req, resp.StatusCode)
+		log.Printf("failed to post message, request: %s, status code: %d\n", string(jsonData), resp.StatusCode)
 		log.Println(resp.Body)
 		return fmt.Errorf("failed to post message, status code: %d", resp.StatusCode)
 	}
